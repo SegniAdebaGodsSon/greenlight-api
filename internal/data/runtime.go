cf0de86a0b8b33a4ef6f94bad948102883079d9f
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-12-14 18:09:49 +0300

    custom JSON decoding body using json.Unmarshaler interface, UnmarshalJSON method...recieve runtime as <int32> mins

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,11 +1,45 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
 type Runtime int32
+
+// IMPORTANT: Because UnmarshalJSON() needs to modify the
+// receiver (our Runtime type), we must use a pointer receiver for this to work
+// correctly. Otherwise, we will only be modifying a copy (which is then discarded when
+// this method returns).
+// accepts: "<> mins"
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unqoutedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unqoutedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Convert the int32 to a Runtime type and assign this to the receiver. Note that we
+	// use the * operator to deference the receiver (which is a pointer to a Runtime
+	// type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+
+	return nil
+}
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
