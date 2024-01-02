8324e32c04ef1af66dc6f51c6574d7715c11856d
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2024-01-02 10:01:11 +0300

    added IP based rate-limiting and command line flags to configure it

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,6 +26,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -45,6 +50,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	flag.CommandLine.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.CommandLine.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maxumum burst")
+	flag.CommandLine.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/lib/pq v1.10.9
 
-require golang.org/x/time v0.5.0 // indirect
+require golang.org/x/time v0.5.0
 
 require (
 	github.com/fatih/color v1.16.0
